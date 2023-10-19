d2ccc4d8c266514837a9a69ce4ae79e9c1ad0cf7
João Pedro Guerra <jpdytz@gmail.com>
2023-10-19 11:29:08 -0300

    perfil jp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import MainPage from './pages/MainPage';
 
 import ProfileExample from './pages/ProfileExample';
 import BrenoProfile from './pages/profiles/BrenoProfile';
+import ProfileJPGuerra from './pages/profiles/ProfileJPGuerra';
 
 const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'));
 
@@ -21,6 +22,7 @@ function App() {
             
             <Route path="/example" element={<ProfileExample/>}/>
             <Route path="/breno" element={<BrenoProfile/>}/>
+            <Route path="/jp" element={<ProfileJPGuerra/>}/>
 
           </Route>
 
